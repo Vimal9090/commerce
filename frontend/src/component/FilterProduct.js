52377c3b16af98703790f46cfc21102caa1777be
vimal sharma <124519612+Vimal9090@users.noreply.github.com>
2023-05-18 02:47:56 +0530

    first commit

---
+++ frontend/src/component/FilterProduct.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import {CiForkAndknife} from "react-icons/ci"
+
+const FilterProduct = () => {
+  return (
+    <div className='text-3xl p-5 bg-yellow-500 rounded-full'>
+        {/* <CiForkAndknife /> */}
+    </div>
+  );
+};
+
+export default FilterProduct;
